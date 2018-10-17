fddcae0ec72fe0ade43fc91aa86ae746d9a7bd57
windguo <webmaster@windguo.com>
2018-10-17 18:01:22 +0800

    1801

--- pages/design/design.js
+++ pages/design/design.js
@@ -19,5 +19,26 @@ Page({
                 wx.hideLoading();
             }
         })
+    },
+  onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
     }
+    return {
+      title: '一笔签名设计,快来申请定制独一无二的签名',
+      path:'/pages/design/design',
+      imageUrl: '',
+      success: (res) => {
+        wx.showToast({
+          content: '分享成功'
+        });
+      },
+      fail: (res) => {
+        wx.showToast({
+          content: '分享失败,原因是' + res
+        });
+      }
+    }
+  },
 })
