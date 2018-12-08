ce67fd268dedcfb135938004e491e0a55982d252
windguo <webmaster@windguo.com>
2018-12-08 14:07:44 +0800

    1407

--- pages/index/index.js
+++ pages/index/index.js
@@ -96,22 +96,29 @@ Page({
     wx.showLoading({});
     // 扫码进入的判断开始
     const _scene = options.scene;
-    // console.log('_scene_scene', _scene);
+    console.log('_scene_scene', _scene);
     if (Boolean(_scene) == true) {
-      // const _scene = decodeURIComponent(options.scene);
-      let _ar = _scene.split('_');
-      let _classid = _ar[0].split('-');
-      let _id = _ar[1].split('-');
-      let _channel = _id[0];
-      switch (_channel) {
-        case 'duanziid':
-          wx.navigateTo({
-            url: '../duanzi_detail/duanzi_detail?classid=' + _classid[1] + '&id=' + _id[1]
-          });
-          break;
-        default:
-          break;
-      };
+      if (_scene.indexOf('start_') == 0){
+        let __scene = _scene.substring(6);
+        console.log('__scene', __scene);
+        wx.switchTab({
+          url: '../' + __scene + '/' + __scene
+        });
+      } else if (_scene.indexOf('classid-') == 0){
+        let _ar = _scene.split('_');
+        let _classid = _ar[0].split('-');
+        let _id = _ar[1].split('-');
+        let _channel = _id[0];
+        switch (_channel) {
+          case 'duanziid':
+            wx.navigateTo({
+              url: '../duanzi_detail/duanzi_detail?classid=' + _classid[1] + '&id=' + _id[1]
+            });
+            break;
+          default:
+            break;
+        };
+      }
     };
     // 扫码进入的判断结束
     var that = this;
